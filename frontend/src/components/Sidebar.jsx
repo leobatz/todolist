2e03f61ddf384f100b56bd7d4ce6c4abbcccb6de
miojo01 <bats.leco@gmail.com>
2026-08-18 12:14:53 -0300

    feat: criando e ajustando sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,17 +1,37 @@
 import { ChevronFirst } from "lucide-react";
 
 export default function Sidebar({ children }) {
+    const sections = [
+        'Lista de Tarefas',
+        'Agenda',
+        'Calculadora',
+        'Financeiro'
+    ];
+    
     return (
-        <aside className="h-screen">
-            <nav className="h-full flex flex-col bg-white border-r shadow-sm">
+        <aside className="h-screen relative z-10 w-[250px] bg-amber-100 shadow-2xl">
+            <nav className="h-full flex flex-col items-center gap-[20px]">
                 <div className="p-4 pb-2 flex justify-between items-center">
-                    <img src="" alt="logo" className="w-32" />
-                    <button className="p-1.5 rounded-lg">
-                        <ChevronFirst />
-                    </button>
+                    <img src="../public/lifehub-logo.png" alt="Logo LifeHub" />
                 </div>
 
-                <ul></ul>
+                <ul>
+                    <li>
+                        <a href="">Lista de Tarefas</a>
+                    </li>
+                    <li>
+                        <a href="">Agenda</a>
+                    </li>
+                    <li>
+                        <a href="">Calculadora</a>
+                    </li>
+                    <li>
+                        <a href="">Financeiro</a>
+                    </li>
+                    <li>
+                        <a href=""></a>
+                    </li>
+                </ul>
             </nav>
         </aside>
     )
