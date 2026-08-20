a585bda6e3446ff0346e705a04dcb987634b188a
miojo01 <bats.leco@gmail.com>
2026-08-20 12:14:07 -0300

    refactor: ajustando app.js com routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,31 @@
-import { useEffect, useState, useRef } from 'react'
 import './App.css'
-import ToDoList from './components/ToDoList'
-import Sidebar from './components/Sidebar'
+import { useEffect, useState, useRef } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router"
+import Tasks from './pages/Tasks'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   
   return (
-    <div className='flex'>
-      <Sidebar />
-      <main className='flex-1'>
-        <ToDoList />
-      </main>
-    </div>
+    <BrowserRouter>
+      <Routes>
+
+        {/* Rota da página de Login */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Páginas com a Sidebar */}
+        <Route element={<Layout />}>
+
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/tarefas" element={<Tasks />} />
+          <Route path="/agenda" element={<Agenda />} />
+          <Route path="/financeiro" element={<Finance />} />
+
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
