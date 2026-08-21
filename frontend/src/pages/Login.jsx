73d782087fe7109d4fa150b5e598bf242fed96b4
miojo01 <bats.leco@gmail.com>
2026-08-21 12:17:47 -0300

    feat: começando a criar página de login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,16 @@
 function Login() {
     return (
-        <div></div>
+        <main className="bg-amber-100 h-screen w-full flex items-center justify-center gap-[10px]">
+            <img className="w-[170px] absolute top-1 m-[10px]" src="/lifehub-logo.svg" alt="Logo" />
+            <div className="border h-[400px] w-[500px]">
+                <h1>
+                    
+                </h1>
+            </div>
+            <div className="border h-[400px] w-[400px]">
+                <h1></h1>
+            </div>
+        </main>
     )
 }
 
