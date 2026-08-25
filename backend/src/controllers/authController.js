e5321bdb14c3657c13c431a12204addfbf53aae7
miojo01 <bats.leco@gmail.com>
2026-08-25 12:39:54 -0300

    feat: criando e começando authController e usuarioController

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,10 @@
+import jwt from 'jsonwebtoken'
+import { Usuario } from '../models/Index.js'
+
+export async function registrar(req, res) {
+    
+}
+
+export async function login(req, res) {
+
+}
---
+++ backend/src/controllers/usuarioController.js
@@ -0,0 +1,13 @@
+import { Usuario } from '../models/Index.js'
+
+export async function buscarUsuario(req, res) {
+    //buscar user
+}
+
+export async function atualizarUsuario(req, res) {
+    //atualizar user
+}
+
+export async function deletarUsuario(req, res) {
+    //deletar user
+}
