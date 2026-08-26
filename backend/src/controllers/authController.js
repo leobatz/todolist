df4cfd5aeeb35a9d0e599251d754d893491e93fd
miojo01 <bats.leco@gmail.com>
2026-08-26 12:23:39 -0300

    feat: começando authController

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,9 @@
 import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+import bcrypt from 'bcrypt'
 import { Usuario } from '../models/Index.js'
+
+dotenv.config()
 
 export async function registrar(req, res) {
     
