b87b0a26d2e1597e400049861d03ac527526f273
miojo01 <bats.leco@gmail.com>
2026-09-01 12:28:05 -0300

    feat: frontend - criando botao de perfil de usuario usercard

---
+++ frontend/src/components/UserCard.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react"
+import { User, Settings, LogOut } from 'lucide-react';
+
+function UserCard() {
+    const [abrirMenu, setAbrirMenu] = useState(false)
+
+    const userOptions = [
+        {
+            name: "Perfil",
+            icon: User
+        },
+        {
+            name: "Configurações",
+            icon: Settings
+        },
+        {
+            name: "Sair",
+            icon: LogOut
+        }
+    ]
+
+    return (
+        <div className="relative">
+            <button onClick={() => setAbrirMenu(!abrirMenu)} className="w-full h-[50px] mb-[10px] flex items-center gap-3 hover:bg-[#dbd1a9] p-[5px] rounded-[10px] transition cursor-pointer">
+                <div className="bg-amber-600 flex items-center h-[40px] w-[40px] justify-center rounded-full p-[5px]">
+                    <p>OB</p>
+                </div>
+                <div>
+                    <h1>oceanbatz</h1>
+                </div>
+            </button>
+
+            {abrirMenu && (
+                <div className="absolute mb-2 w-full bottom-full bg-amber-100 rounded-lg drop-shadow-xl">
+                    {userOptions.map((option) => {
+                        const Icon = option.icon
+
+                        return (
+                            <button className="flex gap-2 w-full h-[35px] p-3 text-left rounded-lg items-center cursor-pointer hover:bg-[#dbd1a9] transition">
+                                <Icon size={20}/>
+                                {option.name}
+                            </button>
+                        )
+                    })}
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default UserCard
