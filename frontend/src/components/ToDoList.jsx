588563d80038acfaea6398e8c841f3a7555449a9
miojo01 <bats.leco@gmail.com>
2026-09-04 12:19:48 -0300

    frontend - ajustando importação da conexão com a api

--- frontend/src/components/ToDoList.jsx
+++ frontend/src/components/ToDoList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useRef } from 'react'
-import api from '../services/api'
+import api from '../services/Api'
 import { SquarePen, Trash2 } from 'lucide-react';
 
 function ToDoList() {
