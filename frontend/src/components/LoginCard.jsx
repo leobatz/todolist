c2ea4910ffb8f573c797cd7527d03e934d99d270
miojo01 <bats.leco@gmail.com>
2026-09-04 12:17:31 -0300

    apenas mudando o import

--- frontend/src/components/LoginCard.jsx
+++ frontend/src/components/LoginCard.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from 'react-router-dom'
 import SuccessAlert from "./Alerts/SuccessAlert";
 import ErrorAlert from "./Alerts/ErrorAlert";
-import api from "../services/api"
+import api from "../services/Api"
 import WarningAlert from "./Alerts/WarningAlert";
 
 function LoginCard({ setIsRegister }) {
