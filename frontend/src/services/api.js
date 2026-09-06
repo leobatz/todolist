56403e0badbf7c263fb19d89caf1437409358f81
miojo01 <bats.leco@gmail.com>
2026-09-06 12:39:41 -0300

    refactor: frontend - refatorando arquivo api e interceptors

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,32 @@
-import api from './BaseService'
+import axios from "axios"
 
-api.defaults.baseURL = "http://localhost:3000"
+const api = axios.create({
+    baseURL: "http://localhost:3000"
+})
+
+api.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem("token")
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+
+        return config
+    }
+)
+
+api.interceptors.response.use(
+    response => response,
+
+    error => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem("token")
+            window.location;href = "/auth"
+        }
+
+        return Promise.reject(error)
+    }
+)
 
 export default api
