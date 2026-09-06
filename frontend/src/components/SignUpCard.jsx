59857b8ac0827e97bc8fe117c60884981d8d41e4
miojo01 <bats.leco@gmail.com>
2026-09-06 12:39:58 -0300

    ajustando importações

--- frontend/src/components/SignUpCard.jsx
+++ frontend/src/components/SignUpCard.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom'
 import { ArrowLeft } from "lucide-react";
-import api from "../services/api"
+import api from "../services/Api"
 
 function SignUpCard({ setIsRegister }) {
     const [name, setName] = useState("")
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom'
-import api from "../services/api"
+import api from "../services/Api"
 import LoginCard from "../components/LoginCard";
 import SignUpCard from "../components/SignUpCard";
 
