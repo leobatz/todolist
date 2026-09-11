dd7d5244c92891a8353cf7ef85b2739d6346f18d
miojo01 <bats.leco@gmail.com>
2026-09-11 12:38:39 -0300

    feat: backend - adicionando novos relacionamentos de eventos

--- backend/src/models/Index.js
+++ backend/src/models/Index.js
@@ -1,5 +1,7 @@
 import Task from './Task.js'
 import Usuario from "./Usuario.js"
+import Evento from './Evento.js'
+import CategoriaEvento from './CategoriaEvento.js'
 
 //Relacionamento Usuario -> Task | Usuario 1:N Task
 Usuario.hasMany(Task, { // Um Usuario pertence e possui muitas Tasks (1:N)
@@ -13,7 +15,33 @@ Task.belongsTo(Usuario, { // Várias Tasks pertencem a um Usuario
     onDelete: 'CASCADE'
 })
 
+//Relacionamento Usuario -> Evento | Usuario 1:N Evento
+Usuario.hasMany(Evento, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+})
+
+Evento.belongsTo(Usuario, {
+    constraint: true,
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+})
+
+//Relacionamento CategoriaEvento -> Evento | CategoriaEvento 1:N Evento
+CategoriaEvento.hasMany(Evento, {
+    foreignKey: 'categoriaEventoId',
+    onDelete: 'CASCADE'
+})
+
+Evento.belongsTo(CategoriaEvento, {
+    constraint: true,
+    foreignKey: 'categoriaEventoId',
+    onDelete: 'CASCADE'
+})
+
 export {
     Usuario,
-    Task
+    Task,
+    Evento,
+    CategoriaEvento
 }
