3f0fa9e0ef751ed3f10fc5b20d2cb5ec50f82592
miojo01 <bats.leco@gmail.com>
2026-09-16 21:04:48 -0300

    refactor: frontend - ajustando sidebar com navigation e location

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,10 @@
 import UserCard from "./UserCard";
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function Sidebar() {
+    const navigate = useNavigate();
+    const location = useLocation();
+
     const sections = [
         'Dashboard',
         'Lista de Tarefas',
@@ -17,11 +21,16 @@ function Sidebar() {
                 </div>
 
                 <ul className="flex flex-col gap-[10px]">
-                    {sections.map((section) => (
-                        <li className="flex w-[200px]" key={section}>
-                            <a className="flex justify-center items-center border h-[35px] w-full font-poppins hover:scale-102 hover:-rotate-2 transition-transform duration-200" href="">{section}</a>
-                        </li>
-                    ))}
+                    {sections.map((section) => {
+                        let nomeSecao = section;
+                        const url = nomeSecao.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().replace(/\s+/g, '');
+
+                        return (
+                            <li onClick={() => {navigate(`/${url}`)}} className="flex w-[200px]" key={section}>
+                                <a href={`/${url}`} className="flex justify-center items-center border h-[35px] w-full font-poppins hover:scale-102 hover:-rotate-2 transition-transform duration-200">{section}</a>
+                            </li>
+                        )
+                    })}
                 </ul>
 
                 <div className="mt-auto w-[200px]">
